f6035865747a2c3e9e2feb40266913e637e742fa
BakPeter <bak.pet@gmail.com>
2023-10-02 11:02:53 +0300

    init

---
+++ StartService.bat
@@ -0,0 +1,2 @@
+docker-compose -f "C:\Workspaces\Eternity\docker-compose-nifi.yml" up -d --build
+docker-compose -f "C:\Workspaces\Eternity\docker-compose-db.yml" up -d --build
---
+++ StopSevices.bat
@@ -0,0 +1,2 @@
+docker compose -f "C:\Workspaces\Eternity\docker-compose-nifi.yml" down
+docker compose -f "C:\Workspaces\Eternity\docker-compose-db.yml" down
